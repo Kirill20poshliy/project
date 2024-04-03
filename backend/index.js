e891033cca3fd433c6ccb438e81af1fc2b303919
Kirill20poshliy <qazasd2014@yandex.ru>
2024-04-03 21:41:32 +0300

    fix-5

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const corsOptions = {
 const app = express()
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://91.236.199.149');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'true, content-type, authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
