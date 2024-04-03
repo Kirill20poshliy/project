185d383a13d9f27b468d51a0d6934f9116a35215
Kirill20poshliy <qazasd2014@yandex.ru>
2024-04-03 22:22:57 +0300

    fix-6.2

--- backend/db/db.config.js
+++ backend/db/db.config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
     host: process.env.POSTGRES_HOST,
-    dialect: process.env.DATABASE_DIALECT,
+    dialect: 'postgres',
     pool: {
         min: 0,
         max: 5,
