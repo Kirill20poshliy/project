b3911f5d965d89c0cf76b4148e0de5e6ea123f52
Kirill20poshliy <qazasd2014@yandex.ru>
2024-04-04 21:47:11 +0300

    bot_v1

--- telebot/index.js
+++ telebot/index.js
@@ -1,51 +1,49 @@
 import { Telegraf } from 'telegraf'
 import 'dotenv/config'
-import shedule from 'node-schedule'
 import messageBuildService from './services/messageBuild.service.js'
+// import shedule from 'node-schedule'
 
+// const rule = new shedule.RecurrenceRule()
+// rule.hour = 23
+// rule.minute = 22
+
+// const messages = []
 const bot = new Telegraf(process.env.BOT_TOKEN)
-const rule = new shedule.RecurrenceRule()
-rule.hour = 23
-rule.minute = 22
 
-const messages = []
-
-bot.start((ctx) => {
-    ctx.reply(`Привет, ${ctx.message.from.first_name}\nВыбери действие`, {
+const reply_keyboard = (ctx) => {
+    ctx.reply(`Выбери действие`, {
         reply_markup: {
             inline_keyboard: [
-                [{text: 'Расписание на сегодня', callback_data: 'btn-1'}],
-                [{text: 'Включить/выключить рассылку', callback_data: 'btn-2'}],
+                [{text: '📅 Расписание на сегодня', callback_data: 'btn-1'}],
             ],
         },        
     })
-    messages.push(ctx.message.message_id)
-})
+}
 
-bot.command('clear', (ctx) => {
-    messages.forEach(message => {
-        ctx.deleteMessage(message) 
-    });
+
+bot.start((ctx) => {
+    reply_keyboard(ctx)
 })
 
 bot.action('btn-1', async (ctx) => {
     await messageBuildService.build().then(msg => {
         ctx.reply(msg, {parse_mode: 'HTML'})
+        reply_keyboard(ctx)
     })
 })
 
-bot.action('btn-2', (ctx) => {
-    ctx.reply(
-`Вы включили рассылку актуального расписания
-Рассылка будет приходить ежедневно в 10:00
-PS: Чтобы отключить данную функцию ${'${description}'}
-`)
-    const job = shedule.scheduleJob(rule, async () => {
-        await messageBuildService.build().then(msg => {
-            ctx.sendMessage(msg, {parse_mode: 'HTML'})
-        })
-    })
-})
+// bot.action('btn-2', (ctx) => {
+//     ctx.reply(
+// `Вы включили рассылку актуального расписания
+// Рассылка будет приходить ежедневно в 10:00
+// PS: Чтобы отключить данную функцию ${'${description}'}
+// `)
+//     const job = shedule.scheduleJob(rule, async () => {
+//         await messageBuildService.build().then(msg => {
+//             ctx.sendMessage(msg, {parse_mode: 'HTML'})
+//         })
+//     })
+// })
 
 bot.launch()
 
